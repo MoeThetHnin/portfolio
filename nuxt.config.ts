286a1824da81cf35e442171c116f4c0c8b7133d2
Moe Thet Hnin <moethethnin.tu@gmail.com>
2025-02-03 22:48:22 +0900

    Add base path in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app:{
+    baseURL: "portfolio"
+  },
   css: ['~/assets/scss/main.scss'],
   postcss: {
     plugins: {
