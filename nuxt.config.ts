dd808038a97a865833f88e0acd84003b6b8a628f
Moe Thet Hnin <moethethnin.tu@gmail.com>
2025-02-03 22:55:25 +0900

    Fixed base path error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app:{
-    baseURL: "portfolio"
+    baseURL: "/portfolio/"
   },
   css: ['~/assets/scss/main.scss'],
   postcss: {
