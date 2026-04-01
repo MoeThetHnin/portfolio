1af977e78d9ee3fd50a5fe2f472a309009588f9f
Moe Thet Hnin <41913942+MoeThetHnin@users.noreply.github.com>
2026-04-02 05:20:26 +0900

    Delete components directory

--- components/Footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <footer class=""><small>COPYRIGHT © MOE THET HNIN</small></footer>
-</template>
-<style scoped lang="scss">
-    @use "tailwindcss";
-    footer{
-        @apply p-3 text-center bg-gray-500
-    }
-</style>
